ace183071501ff6d7cda155154cf4fbe7ddae669
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-03-24 16:33:25 -0300

    feat: get unique industries to test

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,12 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    all_jobs = read(path)
+    industries = []
+    for job in all_jobs:
+        if job["industry"] not in industries:
+            industries.append(job["industry"])
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
