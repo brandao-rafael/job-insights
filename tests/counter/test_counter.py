212596ea8b427d29231a0d84934f0032df0e848d
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-03-27 11:37:16 -0300

    test: for the function test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,22 @@
-# from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+import pytest
+from src.pre_built.counter import count_ocurrences
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def mocked_data():
+    return """
+    banana,
+    Pêra,
+    maçã,
+    Banana,
+    banana,
+    pêra,
+    banana,
+    Banana
+    """
+
+
+def test_counter(mocked_data):
+    with patch("builtins.open", mock_open(read_data=mocked_data)):
+        assert count_ocurrences("/dir/file.csv", "banana") == 5
